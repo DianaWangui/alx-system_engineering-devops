fbae446662dc8e02debc94e5d60066337e902446
DianaWangui <dianawanguikt048@gmail.com>
2024-04-23 00:16:33 +0300

    Added: Using what you did in the task #0, extend your Python script to export data in the JSON format.

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" employee to do to csv """
+
+import requests
+import sys
+import json
+
+
+if __name__ == "__main__":
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    employee_name = ''
+    data = {}
+    list_1 = []
+
+    for user in users.json():
+        if user.get('id') == int(sys.argv[1]):
+            employee_name = user.get('username')
+    for todo in todos.json():
+        if todo.get('userId') == int(sys.argv[1]):
+            list_1.append({
+               "task": todo.get("title"),
+               "completed": todo.get("completed"),
+               "username": employee_name
+            })
+    data.update({sys.argv[1]: list_1})
+
+    filename = f'{sys.argv[1]}.json'
+    with open(filename, mode='w', encoding='utf-8') as file:
+        json.dump(data, file)
