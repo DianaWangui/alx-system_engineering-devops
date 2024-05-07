bf3d03d4e0006ab9f67237d52d58ebfc1e751916
DianaWangui <dianawanguikt048@gmail.com>
2024-05-07 12:51:38 +0300

    pycode error

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,14 +4,15 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """ Queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit. """
+    """ Queries the Reddit API and returns a list containing
+    the titles of all hot articles for a given subreddit. """
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'limit': 100}
+    head = {'User-Agent': 'Mozilla/5.0'}
+    p = {'limit': 100}
     if after:
         params['after'] = after
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=head, params=p, allow_redirects=False)
     response_data = response.json()
 
     if response.status_code != 200:
