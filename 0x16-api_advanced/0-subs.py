f3ef55927ea8fa8f2b4fd898f2690de02bfc11f0
DianaWangui <dianawanguikt048@gmail.com>
2024-05-07 13:09:23 +0300

    pydoc style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" 0-subs module """
+"""0-subs module."""
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """Query the Reddit API and returns the number of subscribers."""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
