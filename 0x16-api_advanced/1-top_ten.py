bb2f905d6cbb288779f942e0db2b4391f42dc057
DianaWangui <dianawanguikt048@gmail.com>
2024-05-07 13:10:34 +0300

    pydoc style

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" 1-top_ten module """
+"""1-top_ten module."""
 import requests
 
 
 def top_ten(subreddit):
-    """ Query the Reddit API and print the title of the first 10 hot posts."""
+    """Query the Reddit API and print the title of the first 10 hot posts."""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
