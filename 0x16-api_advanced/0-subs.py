e492a40be3b42128ecdf0748530a3f600456a8cc
DianaWangui <dianawanguikt048@gmail.com>
2024-05-10 09:38:53 +0300

    make executable

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,7 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    # if response.status_code != 200:
-    #     return 0
+
     try:
         return response.json().get('data').get('subscribers')
     except Exception:
