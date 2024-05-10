f39ab6917bd15bacbfac93112df7062fa9f59963
DianaWangui <dianawanguikt048@gmail.com>
2024-05-10 09:42:12 +0300

    add limit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,11 +7,12 @@ def top_ten(subreddit):
     """Query the Reddit API and print the title of the first 10 hot posts."""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    params = {'limit': 10}
+    response = requests.get(url, headers=headers, params=params)
     response_data = response.json()
     if response.status_code != 200:
         print(None)
         return
-    for post in response_data.get('data').get('children')[:10]:
+    for post in response_data.get('data').get('children'):
         post_title = post.get('data').get('title')
         print(post_title)
