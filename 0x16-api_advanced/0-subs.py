7bb8c0b447a7db6082ddf10c9e60220e6dd3f6ef
DianaWangui <dianawanguikt048@gmail.com>
2024-05-10 09:35:26 +0300

    add try excwption

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,6 +8,9 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    # if response.status_code != 200:
+    #     return 0
+    try:
+        return response.json().get('data').get('subscribers')
+    except Exception:
         return 0
-    return response.json().get('data').get('subscribers')
